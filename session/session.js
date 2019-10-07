dcb712ede45e6d06f7e614f673400716b4e9ba6e
drosser <drosser@users.noreply.github.com>
2019-10-07 09:57:30 +0100

    Add session object to make more sense

---
+++ session/session.js
@@ -0,0 +1,35 @@
+module.exports = class Session {
+  #id;
+  #expires;
+  #data;
+
+  constructor() {
+    id = "";
+    expire = 0;
+    data = {};
+  };
+
+  get getId() {
+    return this.id;
+  }
+
+  set setId(id) {
+    this.id = id;
+  }
+
+  get getExpires() {
+    return this.expires;
+  }
+
+  set setExpires(expires) {
+    this.expires = expires;
+  }
+
+  get getData() {
+    return this.data;
+  }
+
+  set setData(data) {
+    this.data = data;
+  }
+};
--- store/store.js
+++ store/store.js
@@ -1,11 +1,11 @@
 const encoding = require('../encoding/encoding');
+const Session = require('../session/session');
 
 module.exports = class Store {
 
   //TODO : Create session object that is loaded from Cache via Store
   #cache;
-  #sessionID;
-  #sessionData;
+  #session;
   #secret;
 
   static #signatureStart = 28;
@@ -13,11 +13,14 @@ module.exports = class Store {
   static #valueLength = signatureStart + signatureLength;
 
   constructor() {
+    //TODO : Source cache details from config, or passed into store?
     this.cache = new Cache({
       password: "abc",
       addr: "127.0.0.1:1234",
       db: 0
     });
+
+    session = new Session();
 
     //TODO : Load secret from config - Should this be done here? Probably not.
     secret = "";
@@ -29,10 +32,10 @@ module.exports = class Store {
       const validatedSession = await validateEncryptedSessionToken(encryptedSessionToken);
 
       //Get session from cache
-      const session = await getSessionFromCache();
+      const encodedData = await getSessionFromCache();
 
       //Decode session
-      sessionData = await decodeSession(session);
+      session.setData(await decodeSession(encodedData));
 
       //Validate expiration
       const validExpiry = await validateExpiration();
@@ -41,11 +44,11 @@ module.exports = class Store {
     }
 
     //Return session
-    return sessionData;
+    return session.getData();
   };
 
   store() {
-
+    //if session data is empty
   };
 
   #validateEncryptedSessionToken(encryptedSessionToken) {
@@ -56,7 +59,7 @@ module.exports = class Store {
     }
 
     //Get the session id from the encrypted session token
-    sessionID = encryptedSessionToken.substring(0, signatureStart);
+    session.setID(encryptedSessionToken.substring(0, signatureStart));
 
     //Strip the signature from the encrypted session token
     const sig = encryptedSessionToken.substring(signatureStart, encryptedSessionToken.length);
@@ -78,9 +81,9 @@ module.exports = class Store {
     }
   };
 
-  #decodeSession(session) {
+  #decodeSession(encodedData) {
     //decodeBase64
-    const base64Decoded = encoding.decodeBase64(session);
+    const base64Decoded = encoding.decodeBase64(encodedData);
 
     //decode msgpack
     return encoding.decodeMsgpack(base64Decoded);
@@ -91,7 +94,7 @@ module.exports = class Store {
   };
 
   #generateSignature() {
-    const sum = encoding.generateSha1Sum(sessionID + secret);
+    const sum = encoding.generateSha1Sum(session.getId() + secret);
     return encoding.encodeBase64(Buffer.from(sum));
   }
 };
