869d41d225dc14f5d1c5be9b9ee8d1114c33cf88
csciubbach <csciubba@companieshouse.gov.uk>
2020-02-05 12:18:16 +0000

    Put things in lib

---
+++ test/session/session.test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const createSession = require('../../session');
+
+describe("session getting and setting", function () {
+    it("basic initialisation of session object", function () {
+        let session = createSession();
+
+        expect(session.id).to.equal("");
+        expect(session.expires).to.equal(0);
+        expect(session.data).to.be.empty;
+    });
+
+    it("set and get data (other than initialisation) from session object", function () {
+        let id = "Hello, world!";
+        let expires = 5000;
+        let data = "{\"test\": \"I am some data\"}";
+        let session = createSession();
+
+        session.id = id;
+        session.expires = expires;
+        session.data = data;
+
+        expect(session.id).to.equal(id);
+        expect(session.expires).to.equal(expires);
+        expect(session.data).to.equal(data);
+    })
+});
---
+++ test/store/store.test.js
@@ -0,0 +1,44 @@
+const sinon = require("sinon");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const createStore = require("../../store");
+
+describe("session token size", function () {
+
+    let cache = sinon.stub();
+    let config = {
+        secret: "hello world",
+        expiryPeriod: 1000
+    };
+
+    it("session token is too small", function () {
+
+        let tooSmall = "hello";
+
+        let store = createStore(cache, config);
+
+        expect(store.load(tooSmall)).to.be.rejectedWith(Error, "Encrypted session token not long enough");
+    });
+
+    it("session token is too big", function () {
+
+        let tooBig = "this is way too long to be considered a valid encrypted session token but we'll try it anyway";
+
+        let store = createStore(cache, config);
+
+        expect(store.load(tooBig)).to.be.rejectedWith(Error, "Expected signature does not equal actual");
+    });
+
+    it("session token is correct size but invalid", function () {
+
+        let justRight = "this is a string that is exactly the right size for you";
+
+        let store = createStore(cache, config);
+
+        expect(store.load(justRight)).to.be.rejectedWith(Error, "Expected signature does not equal actual");
+    });
+});
