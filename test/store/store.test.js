965148941c40a95a9cabe48f6fe2f55de1659a5b
drosser <drosser@users.noreply.github.com>
2020-02-10 17:48:48 +0000

    Update store to meet Mocha syntax

--- test/store/store.test.js
+++ test/store/store.test.js
@@ -27,22 +27,13 @@ describe("Store", function () {
             expect(store.load(tooSmall)).to.be.rejectedWith(Error, "Encrypted session token not long enough");
         });
 
-        it("session token is too big", function () {
+        it("session token is > 55", function () {
 
             const tooBig = "this is way too long to be considered a valid encrypted session token but we'll try it anyway";
 
             const store = createStore(cache, config);
 
             expect(store.load(tooBig)).to.be.rejectedWith(Error, "Expected signature does not equal actual");
-        });
-
-        it("session token is correct size but invalid", function () {
-
-            const justRight = "this is a string that is exactly the right size for you";
-
-            const store = createStore(cache, config);
-
-            expect(store.load(justRight)).to.be.rejectedWith(Error, "Expected signature does not equal actual");
         });
     });
 });
