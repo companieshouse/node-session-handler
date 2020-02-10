8c6130d8d9d9e14c6f73ea5f3f7266b33efc50dc
drosser <drosser@users.noreply.github.com>
2020-02-10 17:24:29 +0000

    Update mocha syntax for session tests

--- test/session/session.test.js
+++ test/session/session.test.js
@@ -5,29 +5,31 @@ const expect = chai.expect;
 
 const createSession = require("../../lib/session");
 
-describe("session getting and setting", function () {
-    it("basic initialisation of session object", function () {
+describe("Session", function () {
+    describe("session getting and setting", function () {
+        it("basic initialisation of session object", function () {
 
-        const session = createSession();
+            const session = createSession();
 
-        expect(session.id).to.equal("");
-        expect(session.expires).to.equal(0);
-        expect(session.data).to.be.empty;
-    });
+            expect(session.id).to.equal("");
+            expect(session.expires).to.equal(0);
+            expect(session.data).to.be.empty;
+        });
 
-    it("set and get data (other than initialisation) from session object", function () {
+        it("set and get data (other than initialisation) from session object", function () {
 
-        const id = "Hello, world!";
-        const expires = 5000;
-        const data = "{\"test\": \"I am some data\"}";
-        const session = createSession();
+            const id = "Hello, world!";
+            const expires = 5000;
+            const data = "{\"test\": \"I am some data\"}";
+            const session = createSession();
 
-        session.id = id;
-        session.expires = expires;
-        session.data = data;
+            session.id = id;
+            session.expires = expires;
+            session.data = data;
 
-        expect(session.id).to.equal(id);
-        expect(session.expires).to.equal(expires);
-        expect(session.data).to.equal(data);
+            expect(session.id).to.equal(id);
+            expect(session.expires).to.equal(expires);
+            expect(session.data).to.equal(data);
+        });
     });
 });
