83af61a6cee7e7473561d8a7cd1c4b5100bdf10b
drosser <drosser@users.noreply.github.com>
2020-02-10 17:26:23 +0000

    Update mocha syntax for store

--- test/store/store.test.js
+++ test/store/store.test.js
@@ -9,38 +9,40 @@ const expect = chai.expect;
 
 const createStore = require("../../lib/store");
 
-describe("session token size", function () {
+describe("Store", function () {
+    describe("session token size", function () {
 
-    const cache = sinon.stub();
-    const config = {
-        secret: "hello world",
-        expiryPeriod: 1000
-    };
+        const cache = sinon.stub();
+        const config = {
+            secret: "hello world",
+            expiryPeriod: 1000
+        };
 
-    it("session token is too small", function () {
+        it("session token is too small", function () {
 
-        const tooSmall = "hello";
+            const tooSmall = "hello";
 
-        const store = createStore(cache, config);
+            const store = createStore(cache, config);
 
-        expect(store.load(tooSmall)).to.be.rejectedWith(Error, "Encrypted session token not long enough");
-    });
+            expect(store.load(tooSmall)).to.be.rejectedWith(Error, "Encrypted session token not long enough");
+        });
 
-    it("session token is too big", function () {
+        it("session token is too big", function () {
 
-        const tooBig = "this is way too long to be considered a valid encrypted session token but we'll try it anyway";
+            const tooBig = "this is way too long to be considered a valid encrypted session token but we'll try it anyway";
 
-        const store = createStore(cache, config);
+            const store = createStore(cache, config);
 
-        expect(store.load(tooBig)).to.be.rejectedWith(Error, "Expected signature does not equal actual");
-    });
+            expect(store.load(tooBig)).to.be.rejectedWith(Error, "Expected signature does not equal actual");
+        });
 
-    it("session token is correct size but invalid", function () {
+        it("session token is correct size but invalid", function () {
 
-        const justRight = "this is a string that is exactly the right size for you";
+            const justRight = "this is a string that is exactly the right size for you";
 
-        const store = createStore(cache, config);
+            const store = createStore(cache, config);
 
-        expect(store.load(justRight)).to.be.rejectedWith(Error, "Expected signature does not equal actual");
+            expect(store.load(justRight)).to.be.rejectedWith(Error, "Expected signature does not equal actual");
+        });
     });
 });
