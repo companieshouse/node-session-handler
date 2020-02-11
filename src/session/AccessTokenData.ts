d46e6f73a7cd56eeaf836747152bebfb3c9abfad
csciubbach <csciubba@companieshouse.gov.uk>
2020-02-11 13:19:19 +0000

    Added access token data to session

--- src/session/AccessTokenData.ts
+++ src/session/AccessTokenData.ts
@@ -6,9 +6,9 @@ class AccessTokenData {
     refreshToken?: any;
     token?: any;
     tokenType?: any;
-    
+
     constructor(rawData: any) {
-        
+
         this.expiresIn = rawData[SessionKeys.ExpiresIn];
         this.refreshToken = rawData[SessionKeys.RefreshToken];
         this.token = rawData[SessionKeys.AccessToken];
--- src/session/Session.ts
+++ src/session/Session.ts
@@ -1,18 +1,17 @@
 "use strict";
 
 import { SessionData } from "./SessionData";
+import AccessTokenData = require("./AccessTokenData");
 
 class Session {
 
     id: string;
-    expires: number;
-    data: SessionData;
+    accessToken?: AccessTokenData;
 
-    constructor(id: string, expires: number, data: SessionData) {
+    constructor(id: string, accessToken?: AccessTokenData) {
 
         this.id = id;
-        this.expires = expires;
-        this.data = data;
+        this.accessToken = accessToken;
     }
 }
 
