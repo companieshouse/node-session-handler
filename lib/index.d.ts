21693be393250dbc659336a6e479bba56600c09c
Damian Dunajski <ddunajski@companieshouse.gov.uk>
2020-02-25 12:09:46 +0000

    Rebuild distribution

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,8 @@
-import { CookieConfig } from "./config/CookieConfig";
+import { Session, VerifiedSession } from "./session/model/Session";
+import { ISession } from "./session/model/SessionInterfaces";
 import { SessionStore } from "./session/store/SessionStore";
-import * as EitherUtils from "./utils/EitherAsyncUtils";
 import { SessionMiddleware } from "./session/SessionMiddleware";
+import { CookieConfig } from "./config/CookieConfig";
+import * as EitherUtils from "./utils/EitherAsyncUtils";
 import { Maybe, Either } from "purify-ts";
-export { SessionStore, SessionMiddleware, CookieConfig, EitherUtils, Maybe, Either };
+export { Session, VerifiedSession, ISession, SessionStore, SessionMiddleware, CookieConfig, EitherUtils, Maybe, Either };
