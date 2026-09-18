6c1c3d176365ceebc4a9efeef039e61b01ee07dd
John Griffiths <74862880+jgriffiths-ch@users.noreply.github.com>
2026-09-18 12:34:59 +0100

    Bump to force SBOM regeneration

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { SessionStore } from "./session/store/SessionStore";
 import { SessionMiddleware } from "./session/SessionMiddleware";
 import { CookieConfig } from "./config/CookieConfig";
 
-
 declare global {
     namespace Express {
         export interface Request {
